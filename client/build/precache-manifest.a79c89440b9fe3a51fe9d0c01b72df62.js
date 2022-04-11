800a6f57481c8f1f3af2f22f5a2e0cf6b83c6709
Tahsin Ahmed Tushar <mail@devtahsin.com>
2022-04-11 23:27:26 +0600

    CLIENT: BUILD 0.1.4

--- client/build/precache-manifest.a79c89440b9fe3a51fe9d0c01b72df62.js
+++ client/build/precache-manifest.a79c89440b9fe3a51fe9d0c01b72df62.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "4e4fe403b0f97f76004d",
+    "revision": "9c0556b76517238cef98",
     "url": "/static/css/main.93b85f8d.chunk.css"
   },
   {
-    "revision": "4e4fe403b0f97f76004d",
-    "url": "/static/js/main.9946ea87.chunk.js"
+    "revision": "9c0556b76517238cef98",
+    "url": "/static/js/main.d1d4e7c1.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "ec6656c94f9b6accf167",
+    "revision": "442ce79699dcdcd7b8ff",
     "url": "/static/css/2.bc103f5d.chunk.css"
   },
   {
-    "revision": "ec6656c94f9b6accf167",
-    "url": "/static/js/2.86f5d209.chunk.js"
+    "revision": "442ce79699dcdcd7b8ff",
+    "url": "/static/js/2.17b970d2.chunk.js"
   },
   {
     "revision": "1ed478a6b265d4b4f5c26bb063203588",
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/static/media/bootstrap-icons.e4cc83dd.woff"
   },
   {
-    "revision": "c6cb3a3491b38bd781e91aa251459c44",
+    "revision": "60018bba16b5fcab65d6eff2dd64f79d",
     "url": "/index.html"
   }
 ];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83121ca9dfff3661501a4c258f42c8a2.js"
+  "/precache-manifest.a79c89440b9fe3a51fe9d0c01b72df62.js"
 );
 
 workbox.clientsClaim();
