04a06e7a76e288e03fb093cef7867c1fb6628c1b
Tahsin Ahmed Tushar <mail@devtahsin.com>
2022-04-14 16:53:42 +0600

    CLIENT: FIX unwanted re renders when user opens modal

--- client/src/components/DashboardPage.jsx
+++ client/src/components/DashboardPage.jsx
@@ -1,55 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { Button, Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import classes from "../styles/Dashboard.module.css";
 import AlertComponent from "./Alert";
 import Gallery from "./Gallery";
-import ProfileModal from "./ProfileModal";
-import NewDocModal from "./NewDocModal";
-import { useAuth } from "../contexts/AuthContext";
 import { useXhr } from "../hooks/useXhr";
 import { useDocs } from "../contexts/DocsContext";
 import Loading from "./Loading";
 import { toast } from "react-toastify";
+import Header from "./Header";
 
 const DashboardPage = () => {
-    const [showProfile, setShowProfile] = useState(false);
-    const [showNewDocModal, setShowNewDocModal] = useState(false);
-
-    const [shouldLogout, setShouldLogout] = useState(false);
-
     const [shouldGetDocs, setShouldGetDocs] = useState(false);
     const [loading, setLoading] = useState(true);
 
-    const { logout } = useAuth();
-
     const { docs, dispatchDocs } = useDocs();
-
-    const logoutHandler = () => {
-        setShouldLogout(true);
-    };
-
-    const logoutResult = useXhr(
-        shouldLogout,
-        "post",
-        `${process.env.REACT_APP_SERVER_URL}/api/users/logout`
-    );
 
     const getAllDocs = useXhr(
         shouldGetDocs,
         "get",
         `${process.env.REACT_APP_SERVER_URL}/api/docs/`
     );
-
-    useEffect(() => {
-        if (logoutResult && logoutResult.type === "success") {
-            dispatchDocs({ type: "unload" });
-            logout();
-        }
-
-        return () => {
-            setShouldLogout(false);
-        };
-    }, [logoutResult]);
 
     useEffect(() => {
         if (getAllDocs && getAllDocs.type === "success") {
@@ -97,38 +67,7 @@ const DashboardPage = () => {
                 <Container
                     className={`text-light rounded p-2 ${classes.dashboard}`}
                 >
-                    <div className="d-flex justify-content-between">
-                        <h4>Dashboard</h4>
-                        <div>
-                            <Button
-                                className="ms-2"
-                                variant="light"
-                                onClick={() => {
-                                    setShowNewDocModal(true);
-                                }}
-                            >
-                                <i className="bi bi-plus-circle"></i>
-                            </Button>
-                            <Button
-                                variant="light"
-                                className="ms-2"
-                                onClick={() => {
-                                    setShowProfile(true);
-                                }}
-                            >
-                                <i className="bi bi-person"></i>
-                            </Button>
-                            <Button
-                                className="ms-2"
-                                variant="light"
-                                onClick={logoutHandler}
-                            >
-                                <i className="bi bi-box-arrow-right"></i>
-                            </Button>
-                        </div>
-                    </div>
-                    <hr />
-
+                    <Header />
                     <AlertComponent
                         variant="secondary"
                         show={!loading && docs && docs.length < 1}
@@ -141,18 +80,6 @@ const DashboardPage = () => {
                     <Gallery docs={docs} />
                 </Container>
             </Container>
-            <ProfileModal
-                show={showProfile}
-                handleClose={() => {
-                    setShowProfile(false);
-                }}
-            />
-            <NewDocModal
-                show={showNewDocModal}
-                handleClose={() => {
-                    setShowNewDocModal(false);
-                }}
-            />
         </>
     );
 };
--- client/src/contexts/ActionsContext.jsx
+++ client/src/contexts/ActionsContext.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import EditDocModal from "../components/EditDocModal";
 import DeleteDocModal from "../components/DeleteDocModal";
 import MailDocModal from "../components/MailDocModal";
+import NewDocModal from "../components/NewDocModal";
 
 const ActionsContext = React.createContext();
 
@@ -13,12 +14,16 @@ export const ActionsProvider = ({ children }) => {
     const [selectedDoc, setSelectedDoc] = useState({ title: "", _id: "" });
     const [docAction, setDocAction] = useState("");
 
+    const [showNewDocModal, setShowNewDocModal] = useState(false);
     const [showDeleteDocModal, setShowDeleteDocModal] = useState(false);
     const [showEditModal, setShowEditModal] = useState(false);
     const [showMailModal, setShowMailModal] = useState(false);
 
     useEffect(() => {
         switch (docAction.type) {
+            case "add":
+                setShowNewDocModal(true);
+                break;
             case "edit":
                 setShowEditModal(true);
                 setSelectedDoc(docAction.doc);
@@ -47,10 +52,17 @@ export const ActionsProvider = ({ children }) => {
         setSelectedDoc,
         docAction,
         setDocAction,
+        setShowNewDocModal,
     };
     return (
         <ActionsContext.Provider value={value}>
             {children}
+            <NewDocModal
+                show={showNewDocModal}
+                handleClose={() => {
+                    setShowNewDocModal(false);
+                }}
+            />
             <EditDocModal
                 show={showEditModal}
                 doc={selectedDoc}
