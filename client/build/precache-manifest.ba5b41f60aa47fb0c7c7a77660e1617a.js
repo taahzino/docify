c3acb5389be78664fe606d8310ecade130a25744
Tahsin Ahmed Tushar <mail@devtahsin.com>
2022-04-14 07:42:35 +0600

    CLIENT: BUILD

---
+++ client/build/precache-manifest.ba5b41f60aa47fb0c7c7a77660e1617a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dc5dd80d0174a1fd5e1",
+    "url": "/static/css/main.93b85f8d.chunk.css"
+  },
+  {
+    "revision": "6dc5dd80d0174a1fd5e1",
+    "url": "/static/js/main.a52c569b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8d6db7e937a6f71633cb",
+    "url": "/static/css/2.ee86d753.chunk.css"
+  },
+  {
+    "revision": "8d6db7e937a6f71633cb",
+    "url": "/static/js/2.fcb19c01.chunk.js"
+  },
+  {
+    "revision": "e4cc83ddcec34db65724eb56c5230b4c",
+    "url": "/static/media/bootstrap-icons.e4cc83dd.woff"
+  },
+  {
+    "revision": "1ed478a6b265d4b4f5c26bb063203588",
+    "url": "/static/media/bootstrap-icons.1ed478a6.woff2"
+  },
+  {
+    "revision": "62b29881cabbd283cdeeb5280cfff71e",
+    "url": "/index.html"
+  }
+];
