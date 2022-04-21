da5e4a02ee671f9e6aaa57238faa63495e16771e
Tahsin Ahmed Tushar <mail@devtahsin.com>
2022-04-22 02:30:01 +0600

    CLIENT: code refactor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import GuestRoute from "./components/GuestRoute.jsx";
 import Wrappers from "./components/Wrappers.jsx";
